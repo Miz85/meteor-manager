82fdce3aa1616695fcc48323f20e1567626ea116
Nazim2k <nazim@tuukay.com>
2014-12-24 10:29:38 +0100

    Initial commit

---
+++ models/MeteorApp.js
@@ -0,0 +1,11 @@
+// Attempt at a model
+MeteorApp = function(name, process, status){
+	this.name = name;
+	this.process = process;
+	this.status = status;
+};
+
+
+MeteorApp.prototype.test = function() {
+	return "test";
+};
